b0b60838d0d71d9e374577e4851de7293f1b3fc5
Tobias <tobias.ljungstrom@gmail.com>
2016-11-18 14:37:34 +0100

    Add basic structure for React-based frontend

--- src/main/java/SpringController.java
+++ src/main/java/SpringController.java
@@ -1,17 +1,10 @@
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
 
 @Controller
 @EnableAutoConfiguration
 public class SpringController {
-
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringController.class, args);

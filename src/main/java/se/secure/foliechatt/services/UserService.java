5aea41ef934ad847275f643ea7122e88bd8251b2
Henrik <henrikbohlin89@gmail.com>
2016-11-21 11:27:37 +0100

    add userService

---
+++ src/main/java/se/secure/foliechatt/services/UserService.java
@@ -0,0 +1,18 @@
+package se.secure.foliechatt.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+import se.secure.foliechatt.domain.User;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private CrudRepository <User, Long> repo;
+
+    public User saveUser(User user) {
+        return repo.save(user);
+    }
+
+}

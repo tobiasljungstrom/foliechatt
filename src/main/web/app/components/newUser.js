f349b80adebed042f304dd551af4ec63d77ab169
Tobias <tobias.ljungstrom@gmail.com>
2016-11-21 15:23:40 +0100

    Capture form information for new user

--- src/main/web/app/components/newUser.js
+++ src/main/web/app/components/newUser.js
@@ -1,22 +1,42 @@
 var React = require('react');
 
 var NewUser = React.createClass({
+
+    submitNewUser: function() {
+        const newUserForm = document.getElementById('newUserForm');
+
+        const alias = newUserForm.elements['aliasInput'];
+        const email = newUserForm.elements['emailInput'];
+        const password = newUserForm.elements['passwordInput'];
+
+        console.log(alias.value + ' ' + email.value + ' ' + password.value);
+
+
+    },
+
     render: function() {
         return (
-            <form>
+            <form id="newUserForm" className="form">
+                <div className="formTitle">New User</div>
                 <table>
-                    <tr>
-                        <td>Alias:</td>
-                        <td><input type="text" name="aliasInput"/></td>
-                    </tr>
-                    <tr>
-                        <td>E-mail:</td>
-                        <td><input type="email" name="emailInput"/></td>
-                    </tr>
-                    <tr>
-                        <td>Password:</td>
-                        <td><input type="password" name="passwordInput"/></td>
-                    </tr>
+                    <tbody>
+                        <tr>
+                            <td>Alias:</td>
+                            <td><input type="text" name="aliasInput"/></td>
+                        </tr>
+                        <tr>
+                            <td>E-mail:</td>
+                            <td><input type="email" name="emailInput"/></td>
+                        </tr>
+                        <tr>
+                            <td>Password:</td>
+                            <td><input type="password" name="passwordInput"/></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><input type="submit" onClick={this.submitNewUser}/></td>
+                        </tr>
+                    </tbody>
                 </table>
             </form>
         );

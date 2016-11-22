9e73849dcd0a7c40b237ebb58bc7446722f461e7
Tobias <tobias.ljungstrom@gmail.com>
2016-11-22 11:42:51 +0100

    Create form for log in

--- src/main/web/app/components/app.js
+++ src/main/web/app/components/app.js
@@ -1,12 +1,14 @@
 var React = require('react');
 
-var NewUser = require('./newuser');
+var NewUser = require('./newUser');
+var LogIn = require('./logIn');
 
 var App = React.createClass({
     render: function() {
         return (
             <div>
                 <NewUser />
+                <LogIn />
             </div>
         );
     }

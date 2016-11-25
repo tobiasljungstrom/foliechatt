297c4108b64859636d391a8637287abb1a47d9e7
Erik Wiberg <erik.wiberg.87@gmail.com>
2016-11-26 00:31:22 +0100

    refactor example code tochainable promises

--- src/main/web/app/components/cryptoTest.js
+++ src/main/web/app/components/cryptoTest.js
@@ -8,21 +8,12 @@ var u1_pubKey = null;
 var u2_privKey = null;
 var u2_pubKey = null;
 
-var publicKeyArray = [u1_privKey, u2_pubKey];
-
-
 var CryptoTest = React.createClass({
 
-
     componentWillMount: function() {
-        console.log("in component will mount");
-        // web/node_modules/openpgp/src/worker/worker.js
-        const passPhrase = Math.random() * (Math.random() * 1000000000);
-        // ./../../node_modules/openpgp/dist/
-        // { path:'openpgp.worker.js' }
-        /*openpgp.initWorker() // set the relative web worker path
-        openpgp.config.aead_protect = true // activate fast AES-GCM mode (not yet OpenPGP standard)
-*/
+        this.generateTestKeys();
+    },
+    generateTestKeys: function() {
         var options = {
             userIds: [ user_1 ], // multiple user IDs
             numBits: 2048   // default was 4096        // RSA key size
@@ -32,116 +23,71 @@ var CryptoTest = React.createClass({
         openpgp.generateKey(options).then(function(key) {
             u1_privKey = key.privateKeyArmored;
             u1_pubKey = key.publicKeyArmored;
-            console.log("generating key for user 1: ", u1_privKey, u1_pubKey);
+            console.log("Done generating key for user 1"); //: ", u1_privKey, u1_pubKey);
         });
-
-        /*var options2 = {
-            userIds: [ user_2 ], // multiple user IDs
-            numBits: 2048   // default was 4096        // RSA key size
-            // passphrase: 'super long and hard to guess secret'         // protects the private key
-        };*/
 
         openpgp.generateKey(options).then(function(key) {
             u2_privKey = key.privateKeyArmored;
             u2_pubKey = key.publicKeyArmored;
-            console.log("generating key for user 2: ", u2_privKey, u2_pubKey);
+            console.log("Done generating key for user 2"); // : ", u2_privKey, u2_pubKey);
         });
-
-
-
     },
 
-    getPubPrivKey: function(user) {
-        const passPhrase = Math.random() * (Math.random() * 1000000000);
-        openpgp.initWorker({ path:'openpgp.worker.js' }) // set the relative web worker path
-        openpgp.config.aead_protect = true // activate fast AES-GCM mode (not yet OpenPGP standard)
 
-        var options = {
-            userIds: [ user ], // multiple user IDs
-            numBits: 4096,                                            // RSA key size
-            passphrase: 'super long and hard to guess secret'         // protects the private key
-        };
-
-
-        openpgp.generateKey(options).then(this.log("my message")).then(this.encrypt).then(mySendFunction)
-
-       /* openpgp.generateKey(options).then(function(key) {
-            u1_privKey = key.privateKeyArmored; // '-----BEGIN PGP PRIVATE KEY BLOCK ... '
-            u1_pubKey = key.publicKeyArmored;   // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
-        });*/
-
-    },
-
-    encrypt: function(message, callback) {
+    encrypt: function(message, senderPrivateKey, receiverPublicKey) {
         console.log("inside encrypt promise");
 
         options = {
             data: message,                             // input as String (or Uint8Array)
-            publicKeys: openpgp.key.readArmored(u2_pubKey).keys,  // for encryption
-            privateKeys: openpgp.key.readArmored(u1_privKey).keys // for signing (optional)
+            publicKeys: openpgp.key.readArmored(receiverPublicKey).keys,  // for encryption
+            privateKeys: openpgp.key.readArmored(senderPrivateKey).keys // for signing (optional)
         };
 
-        openpgp.encrypt(options).then( function(cipherText) {
-            callback(cipherText);
-        });
-
-        /*.then(function(ciphertext) {
-            sendCallback(ciphertext.data);// '-----BEGIN PGP MESSAGE ... END PGP MESSAGE-----'
-        });*/
-
+        return openpgp.encrypt(options); // returns a promise.. pass callback to receive encrypted Message, this.encrypt.then( (encryptedMessage) => ?? do stuff )
     },
 
-    log: function(message) {
-        return (function(thingToLog) {
-            console.log(message, thingToLog);
-        });
-    },
-    decrypt: function(message) {
-    /*    let options = {
-            message: openpgp.message.readArmored(message),     // parse armored message
-            publicKeys: openpgp.key.readArmored([u1_pubKey, u2_pubKey]).keys,    // for verification (optional)
-            privateKey: openpgp.key.readArmored(u1_privKey).keys[0] // for decryption
-        };
-
-        openpgp.decrypt(options).then(function(plaintext) {
-            console.log("inside decrypt callback. plaintext is: ", plaintext);
-            //return plaintext.data; // 'Hello, World!'
-        });
-*/
-
-
-        let options2 = {
-            message: openpgp.message.readArmored(message),     // parse armored message
-            publicKeys: openpgp.key.readArmored(u1_pubKey).keys,    // for verification (optional)
-            privateKey: openpgp.key.readArmored(u2_privKey).keys[0] // for decryption
-        };
-
-        openpgp.decrypt(options2).then(function(plaintext) {
-            console.log("inside decrypt 2 callback. plaintext is: ", plaintext);
-            //return plaintext.data; // 'Hello, World!'
+    logWithMessage: function(message) {
+        // return a function ready to take one thing and log it (with a predefined message), then pass it on.
+        return (function logMe(thingToLog) {
+            console.log("------------------\n" + message, "\n" , thingToLog, "\n----------------");
+            return thingToLog;
         });
     },
 
-    sendMessage: function() {
+    decryptWithKeys: function(publicSenderKey, privateReceiverKey) {
+        // return a function taking just the message as an arg, with the key variables bound. (good for chaining. see testEncryptAndDecryptMessage)
+        return (function decryptMessage(message) {
+            let options2 = {
+                message: openpgp.message.readArmored(message),     // parse armored message
+                publicKeys: openpgp.key.readArmored(publicSenderKey).keys,    // for verification (optional)
+                privateKey: openpgp.key.readArmored(privateReceiverKey).keys[0] // for decryption
+            };
+
+            return openpgp.decrypt(options2); // returns a promise.. pass callback to receive encrypted Message, this.decrypt.then( (decryptedMessage) => ?? do stuff )
+        });
+    },
+
+    testEncryptAndDecryptMessage: function() {
         let message = document.getElementById("sendMe").value;
-        let decryptFunc = this.decrypt;
 
-        this.encrypt(message, function(encryptedText) {
-
-            console.log("encrypted message is ",encryptedText.data);
-            decryptFunc(encryptedText.data);
-
-        });
-
-
+        this.encrypt(message, u1_privKey, u2_pubKey)
+            .then(this.logWithMessage("Encrypted message (object) is: "))
+            .then( message => message.data )  // transform, because the next method needs just the data
+            .then(this.logWithMessage("Just the encrypted data: "))
+            // Later, do sending logic here..  then receive and decrypt in another function
+            .then(this.decryptWithKeys(u1_pubKey, u2_privKey)) // return a new one arg function that only takes last parameter of .decrypt()
+            .then(this.logWithMessage("Decrypted message (object) is: "))
+            .then(message => message.data)
+            .then(this.logWithMessage("Just the decrypted data: "));
 
     },
 
     render: function() {
         return (
             <div>
+                <h4>Encryption test</h4>
                 <input id="sendMe" />
-                <button onClick={ this.sendMessage }>Send</button>
+                <button onClick={ this.testEncryptAndDecryptMessage }>Send</button>
             </div>
         );
     }

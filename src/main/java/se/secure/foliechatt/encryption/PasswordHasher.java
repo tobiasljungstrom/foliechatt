568bd89675e10ad8e094146d3877b56ec63c2d16
Tobias <tobias.ljungstrom@gmail.com>
2016-11-25 16:19:34 +0100

    Add validiation method to PasswordHasher

--- src/main/java/se/secure/foliechatt/encryption/PasswordHasher.java
+++ src/main/java/se/secure/foliechatt/encryption/PasswordHasher.java
@@ -27,24 +27,45 @@ public class PasswordHasher {
 
     }
 
-    private byte[] doSalt() throws NoSuchAlgorithmException
-    {
+    private boolean validatePassword(String incomingPassword, PasswordWrapper storedPassword) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        int iterations = storedPassword.getIterations();
+        byte[] salt = fromHex(storedPassword.getSalt());
+        byte[] hash = fromHex(storedPassword.getPassword());
+
+        PBEKeySpec spec = new PBEKeySpec(incomingPassword.toCharArray(), salt, iterations, hash.length * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] testHash = skf.generateSecret(spec).getEncoded();
+
+        int diff = hash.length ^ testHash.length;
+        for (int i = 0; i < hash.length && i < testHash.length; i++) {
+            diff |= hash[i] ^ testHash[i];
+        }
+        return diff == 0;
+    }
+
+    private byte[] doSalt() throws NoSuchAlgorithmException {
         SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
         byte[] salt = new byte[32];
         sr.nextBytes(salt);
         return salt;
     }
 
-    private static String toHex(byte[] array) throws NoSuchAlgorithmException
-    {
+    private String toHex(byte[] array) throws NoSuchAlgorithmException {
         BigInteger bi = new BigInteger(1, array);
         String hex = bi.toString(16);
         int paddingLength = (array.length * 2) - hex.length();
-        if(paddingLength > 0)
-        {
-            return String.format("%0"  +paddingLength + "d", 0) + hex;
-        }else{
+        if (paddingLength > 0) {
+            return String.format("%0" + paddingLength + "d", 0) + hex;
+        } else {
             return hex;
         }
+    }
+
+    private byte[] fromHex(String hex) throws NoSuchAlgorithmException {
+        byte[] bytes = new byte[hex.length() / 2];
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
+        }
+        return bytes;
     }
 }
